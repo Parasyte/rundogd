282a6d37857d4c4fc15e541a1147ce953be29e31
Jay Oster <jay@pubnub.com>
2016-10-28 15:32:38 -0700

    Update copyright date

--- rundogd.py
+++ rundogd.py
@@ -1,4 +1,4 @@
-# Copyright 2011 Jason Oster. All rights reserved.
+# Copyright 2011-2016 Jason Oster. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
